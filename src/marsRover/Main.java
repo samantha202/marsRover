eac03b7cc676fd7303cca1d227e6f610f5117448
samantha <nsamanthaduclaire@gmail.com>
2022-09-18 22:40:29 +0200

    trying to understand the subject

--- src/marsRover/Main.java
+++ src/marsRover/Main.java
@@ -1,5 +1,21 @@
 package marsRover;
 
+    //Rover : Position and location represented by (x, y, z), where Z in {N, E, W, S}
+	//plateau: Grid of position following the format (x,y,z) where (0, 0, N) => (x=0, y=0, Z=N)
+	//	       MaximumCoordinates => (maxX, maxY) => (5 5)
+	// Message: (LLLRRMM)  => (L=SpinLeft, SpinRight, M=MovedForward)
+	// Input = 5 lines:
+	// 1. Plateau Size: (5 5)
+	// 2. Array of RoverInstruction objects, where RoverInstruction object contains:
+	//		a. RoverPosition (first line)
+	//		b. RoverCommand (second line)
+	
+	// Expected Behaviors:
+	// 1. Rover Should Spin Left
+	// 2. Rover Should Spin Right
+	// 3. Rover Should Spin Forward	
+    // 4. Rover Should Move
+	   
 public class Main {
 
 	public static void main(String[] args) {
