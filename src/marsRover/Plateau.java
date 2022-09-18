f6e27f5120daa3426ba74ddfd1adb7944da474f1
samantha <nsamanthaduclaire@gmail.com>
2022-09-18 22:46:40 +0200

    plateau class created

---
+++ src/marsRover/Plateau.java
@@ -0,0 +1,16 @@
+package marsRover;
+
+public class Plateau {
+	
+	public int upper_limit_x;
+	public int upper_limit_y;
+	public int lower_limit_x = 0;
+	public int lower_limit_y = 0;
+	
+	public Plateau(int upper_limit_x, int upper_limit_y) {
+		super();
+		this.upper_limit_x = upper_limit_x;
+		this.upper_limit_y = upper_limit_y;
+	}	
+}
+
